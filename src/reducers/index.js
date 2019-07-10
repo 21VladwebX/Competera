18bf500c3b2e201f69eb616705d5433267cd6a8b
21VladwebX <valdekkrutek@gmail.com>
2019-07-10 17:26:29 +0300

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+// import weather from './weather_data';
+
+
+// export default combineReducers({
+//   weather
+// })
+
+export default function weather(state = [], action){
+
+//   if(action.type === 'CHANGE_WEATHER'){
+
+//     return{
+//       temp: action.temp,
+//       hum: action.hum,
+//       weath: action.weath,
+//       city: action.city
+//   }
+//   }
+  return state;
+}
